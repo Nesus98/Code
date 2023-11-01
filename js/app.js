d2ece360b4acdf75f021d87f16bcd88ad34a53cf
Nesus98 <jesusarroyo30.98@gmail.com>
2023-11-01 13:03:33 +0100

    sesion 31

--- js/app.js
+++ js/app.js
@@ -6,15 +6,18 @@ window.addEventListener("load", () => {
   let close = alert.firstElementChild;
   let input = document.querySelector("#task");
   let arrow = document.querySelector(".arrow");
-  let table =
-    // console.log(input);
+  let done = document.querySelectorAll(".fa-circle-check");
+  let trash = document.querySelectorAll(".fa-trash");
+  let edit = document.querySelectorAll(".fa-pencil");
+  let task = document.querySelectorAll(".task");
+  // console.log(input);
 
-    //Events
-    //Close the alert
+  //Events
+  //Close the alert
 
-    close.addEventListener("click", () => {
-      alert.classList.add("dismissible");
-    });
+  close.addEventListener("click", () => {
+    alert.classList.add("dismissible");
+  });
   input.addEventListener("focus", () => {
     document.addEventListener("keydown", (e) => {
       // console.log(event.code);
@@ -36,20 +39,77 @@ window.addEventListener("load", () => {
       id =
         Number(document.querySelector("tbody")?.lastElementChild?.id) + 1 || 0;
       console.log(id);
-      
+
       document.querySelector("tbody").appendChild(generateRow(id, text));
       if (!alert.classList.contains("dismissible")) {
         alert.classList.add("dismissible");
       }
     }
   });
+  
+  done.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      deleteTask(e);
+    });
+  });
+  
+  trash.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      // deleteTask(e);
+      removeRow(e);
+    });
+  });
+
+  edit.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      editTask(e, false);
+    });
+  });
+
+  task.forEach((item) => {
+    item.addEventListener("focus", (e) => {
+      editTask(e, true);
+    });
+  });
+
 });
 
-const generateRow = (id, text) => {
-//Creando una nueva fila
-let newRow = document.createElement("tr");
-newRow.setAttribute("id", id);
-newRow.innerHTML = `
+let editTask = (e, onFocus) => {
+
+  if (onFocus) {
+    e.target.classList.add("editable");
+  } else {
+    let editable = e.target.parentNode.parentNode.previousElementSibling.lastElementChild;
+    console.log(editable);
+    editable.classList.add("editable");
+    editable.focus();
+  } 
+  };
+
+let deleteTask = (e) => {
+  let task = e.target.nextElementSibling;
+  text = task.innerHTML;
+
+  if (text.includes("<del>")) {
+    task.innerHTML = task.firstElementChild.textContent;
+    task.setAttribute("data.complete", "false");
+  } else {
+    task.innerHTML = `<del>${text}</del>`;
+    task.setAttribute("data.complete", "true");
+  }
+};
+
+let removeRow = (e) => {
+  e.target.parentNode.parentNode.parentNode.remove();
+};
+
+//Refactorizamos el codigo encapsulando la funcion
+let generateRow = (id, text) => {
+  e;
+  //Creando una nueva fila
+  let newRow = document.createElement("tr");
+  newRow.setAttribute("id", id);
+  newRow.innerHTML = `
   <td>
           <i class="fa-solid fa-circle-check fa-2x"></i>
           <span class="task" contenteditable="true"> ${text} </span>
